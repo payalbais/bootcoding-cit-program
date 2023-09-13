dea0061aae843454d174145c8ef8d5814ae951ef
payalbais <144772437+payalbais@users.noreply.github.com>
2023-09-13 13:14:33 +0530

    Add files via upload

---
+++ MyProgram1.java
@@ -0,0 +1,12 @@
+public class MyProgram1
+{
+	static void myMethod()
+	
+	{
+		System.out.println("I just got Executed");
+	}
+	public static void main(String args[])
+	{
+		myMethod();
+	}
+}
---
+++ Palindrom.java
@@ -0,0 +1,11 @@
+public class Palindrome{
+public static void main(String args[]){
+int n=1234;
+int reverse =0;
+while(n>0){
+int remainder=n%10;
+reverse=(reverse*10)+remainder;
+n=n/10;
+}
+}
+}
